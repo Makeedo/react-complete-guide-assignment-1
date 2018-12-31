7ca327abd28e4bc33d177c6a544793e5d6c1eb72
Nicolaus Klinger <nicolaus.klinger@paysafe.com>
2018-12-31 23:42:05 +0100

    resolved step 10

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const UserInput = (props) => {
 
+    const style = {
+        backgroundColor: 'white',
+        width: '200px',
+        margin: '10px auto',
+        textAlign: 'center',
+        border: '1px solid #ccc',
+        color: 'black',
+        padding: '5px'
+
+    };
+
     return(
-        <input type='text' onChange={props.changeHandler} value={props.value}/>
+        <input type='text' onChange={props.changeHandler} value={props.value} style={style}/>
     );
 
 };
--- src/components/UserOutput/UserOutput.js
+++ src/components/UserOutput/UserOutput.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './UserOutput.css'
 
 const UserOutput = (props) => {
 
     return(
-        <div>
+        <div className='UserOutput'>
             <p>This is a UserOutput component</p>
             <p>UserName = {props.userName}</p>
         </div>
