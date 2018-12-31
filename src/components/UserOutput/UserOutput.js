9a41210bca00937cb942948e4df3d9fc06e9313f
Nicolaus Klinger <nicolaus.klinger@paysafe.com>
2018-12-31 23:10:46 +0100

    resolved step 1 - 3

---
+++ src/components/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UserOutput = (props) => {
+
+    return(
+        <div>
+            <p>This is a UserOutput component</p>
+            <p>It holds two paragraphs</p>
+        </div>
+    );
+
+};
+
+export default UserOutput;
